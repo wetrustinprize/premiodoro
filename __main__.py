ff34574958e9848163902389f7e1daffa329123e
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-01-18 17:48:10 -0300

    classes

--- __main__.py
+++ __main__.py
@@ -1,76 +1,37 @@
-from blessed import Terminal
-from pynput.mouse import Controller
-from pynotifier import Notification
+from config import PREMIODORO_OUT
 
-from config import REMINDERS, PREMIODORO_OUT, IDLE_MAXCOUNT, IDLE_USE
-
-from reminder import Reminder
-from display import Display
-
-from playsound import playsound
+from premiodoro.reminder import Reminder
+from premiodoro.display import Display
+from premiodoro.idlemanager import IdleManager
+from premiodoro.remindermanager import ReminderManager
 
 import lockfile
 import os
 import time
 
-mouse = Controller()
-
-# VARIABLES
-
-# The mouse position is used to calculate if the user is in the Computer
-# TODO: Maybe find a better way to check this?
-last_mouse_pos = (0, 0)
-total_idles = 0
-def is_idle(): return total_idles >= IDLE_MAXCOUNT if IDLE_USE else False
-
-
 # Check if var folder exists
 if not os.path.exists('./var'):
     os.mkdir('./var')
 
-# File where the Premiodoro information is stored
-premio_out = None
-if PREMIODORO_OUT != None:
-    premio_out = open(PREMIODORO_OUT, 'w+')
-
-
-def notf(o):
-    Notification(
-        title='PremioDoro Reminder',
-        description=o.message,
-        urgency='normal'
-    ).send()
-
-
-reminders = [Reminder(r['message'], r['period']) for r in REMINDERS]
-for reminder in reminders:
-    reminder.on_complete(
-        lambda:
-            playsound('./notification.wav', False)
-    )
-
+reminders = ReminderManager()
 display = Display()
+idle = IdleManager()
 
 with lockfile.FileLock('./var/premiodoro', timeout=1):
     try:
         while True:
             with display.term.cbreak(), display.term.hidden_cursor():
                 # Logic
-                if last_mouse_pos == mouse.position:
-                    total_idles = min(total_idles + 1, IDLE_MAXCOUNT)
-                else:
-                    total_idles = 0
-                last_mouse_pos = mouse.position
+                idle.check()
 
-                for reminder in reminders:
-                    if is_idle():
-                        reminder.untick(1)
-                    else:
-                        reminder.tick(1)
+                if idle.is_idle:
+                    reminders.untick_all(1)
+                else:
+                    reminders.tick_all(1)
 
                 # Prints
                 display.main(
-                    idle=is_idle(),
+                    idle=idle.is_idle,
                     reminders=reminders
                 )
 
