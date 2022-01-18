ad59efb6097a28d4e9a447ee9d4c4fb9bf952ba4
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-01-18 17:48:07 -0300

    reminders now is a json

--- config.py
+++ config.py
@@ -1,29 +1,8 @@
-####
-# REMINDERS
-####
-# Each reminder has the following keys:
-#   reminder: The message displayed in the notification
-#   period: The period on seconds to display the reminder
-REMINDERS = [
-    {
-        "message": "Hello world!",
-        "period": 5
-    },
-    {
-        "message": "Go take a walk",
-        "period": 3600
-    },
-    {
-        "message": "Look something else for 20 seconds",
-        "period": 1200
-    }
-]
-
 # Output file dir
 # The file where Premiodoro will output information
 # This file can be used in other scripts
 # If set to None, there will be no output written
-PREMIODORO_OUT = './var/premiodoro'
+PREMIODORO_OUT = './var/premiodoro.json'
 
 ####
 # IDLE
